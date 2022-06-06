a9f873a19c3701f764377e7dd60720bf5d1710fe
yury.melnikau <yury.melnikau@gmail.com>
2022-06-06 17:31:08 +0300

    -

--- encyclopedia/src/components/tables/TableLastShortResult.tsx
+++ encyclopedia/src/components/tables/TableLastShortResult.tsx
@@ -1,6 +1,7 @@
 import Table from 'react-bootstrap/Table';
 import React from "react";
 import { IShortResult } from '../../types/types';
+import ReactCountryFlag from "react-country-flag";
 
 interface ResultsListProps {
     result: IShortResult[]
@@ -13,7 +14,15 @@ const TableLastShortResult: React.FC<ResultsListProps> = ({result}) => {
                 {result.map(res =>
                     <tr key={res.racerFullName}>
                         <td>{res.position}</td>
-                        <td>{res.racerFullName}</td>
+                        <td>
+                            <ReactCountryFlag
+                                countryCode="US"
+                                svg
+                                style={{
+                                    width: '25px',
+                                    height: '17px',
+                                }}
+                        /><h4>{res.racerFullName}</h4></td>
                         <td>{res.constructor}</td>
                         <td>{res.timeGap}</td>
                         <td>{res.points}</td>
